eed466e8c0a9f078e6cfeecf96158fda5e490cd2
kwakseobang <iii6602@naver.com>
2025-09-01 14:37:37 +0900

    git initialize

---
+++ src/main/java/org/seasonthon/fakecheck/FakecheckApplication.java
@@ -0,0 +1,13 @@
+package org.seasonthon.fakecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakecheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakecheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/seasonthon/fakecheck/FakecheckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seasonthon.fakecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakecheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
