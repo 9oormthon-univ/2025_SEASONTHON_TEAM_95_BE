94bb0622cdc739bed17a2fb94fbf33918862552e
kwakseobang <iii6602@naver.com>
2025-09-02 19:27:51 +0900

    Feat/#1 : 분석 대시보드 조회 기능

--- src/main/java/org/seasonthon/fakecheck/repository/AnalysisRepository.java
+++ src/main/java/org/seasonthon/fakecheck/repository/AnalysisRepository.java
@@ -1,8 +1,19 @@
 package org.seasonthon.fakecheck.repository;
 
+import java.time.LocalDateTime;
 import org.seasonthon.fakecheck.domain.Analysis;
+import org.seasonthon.fakecheck.enums.RiskLevel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
+    // 오늘 전체 검사 수
+    Long countByCreateTimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime);
+
+    // 오늘 특정 위험도별 카운트
+    Long countByCreateTimeBetweenAndRiskLevel(
+            LocalDateTime startDateTime,
+            LocalDateTime endDateTime,
+            RiskLevel riskLevel
+    );
 
 }
